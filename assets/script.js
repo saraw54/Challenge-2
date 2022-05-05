c33bb9098b2b2557da2ea2ab7e6073650e12219e
Sara Stewart <saraw54@gmail.com>
2022-05-05 12:42:29 -0600

    added most of script

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,43 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+confirm("Do you want to generate a new password?");
+alert("Enter the length of password (must be between 8 and 128 characters)");
+alert("Check box for uppercase letters");
+alert("Check box for lowercase letters");
+alert("Check box for numbers");
+alert("Check box for special characters")
+
+function generatePassword() {
+  var passLength = document.querySelector("#length").value;
+  var upper = document.querySelector("#letters1").checked;
+  var lower = document.querySelector("#letters2").checked;
+  var nums = document.querySelector("#numbers").checked;
+  var specialCharacters = document.querySelector("#specialChar").checked;
+  var upperStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerStr = upperStr.toLowerCase();
+  var numStr = "0123456789";
+  var specialCharStr = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+  var resultsStr = ""
+  var combined = "";
+  if (upper) {
+    combined += upperStr;
+  }
+  if (lower) {
+    combined += lowerStr;
+  }
+  if (nums) {
+    combined += numStr;
+  }
+
+  if (specialCharacters) {
+    combined += specialCharStr;
+  }
+  for (var ii = 0; ii < passLength; ii++) {
+    resultsStr += combined.charAt(Math.floor(Math.random() * combined.length));
+    return resultsStr;
+  }
+}
+
+
